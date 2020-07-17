54d7c15261dbda1e98d3651717fac9a693f0012a
Zaina Rodney <zainarodney@gmail.com>
2020-07-16 20:51:25 -0400

    completed first test

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,12 @@
 // YOUR CODE HERE
+class $ {
+  constructor(str) {
+    elements: [];
+    this.getElements(str);
+  }
+
+  getElements(str) {
+      this.elements = [...document.getElementsByClassName(str.slice(1))];
+  }
+
+}
